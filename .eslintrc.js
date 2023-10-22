21cba4f24b13f4aad087145964cd604d48ae4611
Aravindan Ve <aravindan.ve@outlook.com>
2023-10-22 17:41:15 +0530

    init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["unused-imports"],
+  ignorePatterns: [
+    ".eslintrc.js",
+    "/node_modules",
+    "/package-lock.json",
+    "/yarn.lock",
+    "/.gen",
+    "/.expo",
+    "/.idea",
+    "/assets",
+    "/build",
+    "/coverage",
+    "/dist",
+  ],
+  rules: {
+    // use plugin `unused-imports` instead of `no-unused-vars` rule to auto fix unused imports
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+};
